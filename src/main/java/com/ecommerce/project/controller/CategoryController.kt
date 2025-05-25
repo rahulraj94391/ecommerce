120d908fe156924fd211a0078157396a76125dc3
rahul-18968 <rahulraj94391@gmail.com>
2025-05-25 23:52:27 +0530

    finished video 66

--- src/main/java/com/ecommerce/project/controller/CategoryController.kt
+++ src/main/java/com/ecommerce/project/controller/CategoryController.kt
@@ -1,23 +1,25 @@
 package com.ecommerce.project.controller
 
 import com.ecommerce.project.model.Category
+import com.ecommerce.project.service.CategoryService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController()
-class CategoryController {
-    private val categories: MutableList<Category> = mutableListOf()
-
+class CategoryController(
+    @Autowired private val categoryService: CategoryService
+) {
     @GetMapping("api/public/categories")
     fun getAllCategories(): List<Category> {
-        return categories
+        return categoryService.allCategories
     }
 
     @PostMapping("api/public/categories")
     fun createCategory(@RequestBody category: Category): String {
-        categories.add(category)
+        categoryService.createCategory(category)
         return "Category added successfully"
     }
 }
---
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CategoryServiceImpl implements CategoryService {
+    private final List<Category> categories = new ArrayList<>();
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categories.add(category);
+    }
+}

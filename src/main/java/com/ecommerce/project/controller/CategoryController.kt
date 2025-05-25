3e37b13b4815a29998da5948ca619abb5af7ca5e
rahul-18968 <rahulraj94391@gmail.com>
2025-05-25 22:18:59 +0530

    finished video 65

---
+++ src/main/java/com/ecommerce/project/controller/CategoryController.kt
@@ -0,0 +1,26 @@
+package com.ecommerce.project.controller
+
+import com.ecommerce.project.model.Category
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController()
+class CategoryController {
+    private val categories: MutableList<Category> = mutableListOf()
+
+    @GetMapping("api/public/categories")
+    fun getAllCategories(): List<Category> {
+        return categories
+    }
+
+
+    @PostMapping("api/public/categories")
+    fun createCategory(@RequestBody category: Category): String {
+        categories.add(category)
+        return "Category added successfully"
+    }
+}
+
---
+++ src/main/java/com/ecommerce/project/model/Category.kt
@@ -0,0 +1,6 @@
+package com.ecommerce.project.model
+
+data class Category(
+    val categoryId: Long,
+    val categoryName: String
+)
